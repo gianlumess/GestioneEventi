1a1d877739d04dbe7838683f43e49af983fcbdc8
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-08-20 14:53:27 +0200

    creato collegamento al DB

--- src/main/java/gianlucamessina/Application.java
+++ src/main/java/gianlucamessina/Application.java
@@ -1,6 +1,10 @@
 package gianlucamessina;
 
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("BE_U1_S3_L2");
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
